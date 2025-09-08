2b8837ce605041290f0c259c799993a6dbe781a2
flypt7 <tomsarrazin01@gmail.com>
2025-09-08 19:42:18 -0400

    added overload for multiples method in Multiples. Changed multiples call in FizzBuzz to have no arguments

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,8 +3,21 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
 
-        int count = multiples(1000, 3, 5);
+        int count = multiples();
         System.out.println(count);
+    }
+
+    public static int multiples() {
+        int i = 1;
+        int count = 0;
+
+        while (i < 1000) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count = count + 1;
+            }
+            i = i + 1;
+        }
+        return count;
     }
 
     public static int multiples(int n, int a, int b) {
